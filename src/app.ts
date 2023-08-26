7541beadaca780e6c91985ed163714a8f331f372
Guilherme Nascimento <guilherme.rdnascimento@gmail.com>
2023-08-26 12:41:58 -0300

    setup project

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import fastify from "fastify";
+import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
+
+export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
+app.register(appRoutes);
+
+app.setErrorHandler((err, _, res) => {
+  if (err instanceof ZodError) {
+    return res
+      .status(400)
+      .send({ message: "Validation error", issues: err.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(err);
+  } else {
+    // TODO: there should be a log to an external tool like DataDog
+  }
+
+  return res.status(500).send({ message: "Internal server error" });
+});

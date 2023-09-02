ac691089643e1ad998a5c71e3732cbac3e33a41d
Guilherme Nascimento <guilherme.rdnascimento@gmail.com>
2023-09-02 16:53:05 -0300

    update files name

--- src/services/factories/index.ts
+++ src/services/factories/index.ts
@@ -1,8 +1,8 @@
-import { makeRegisterService } from "./make-create-user-service";
+import { makeRegisterService } from "./make-register-service";
 import { makeCreateProfessionalService } from "./make-create-professional-service";
 import { makeCreateSpecialtyService } from "./make-create-specialty-service";
 import { makeAuthenticateService } from "./make-authenticate-service";
-import { makeSearchSpecialtiesService } from "./make-specialties-service";
+import { makeSearchSpecialtiesService } from "./make-search-specialties-service";
 
 export {
   makeRegisterService,
--- src/services/factories/make-search-specialties-service.ts
+++ src/services/factories/make-search-specialties-service.ts
@@ -1,5 +1,5 @@
 import { PrismaSpecialtiesRepository } from "@/repositories/prisma";
-import { SearchSpecialtiesService } from "../";
+import { SearchSpecialtiesService } from "..";
 
 export function makeSearchSpecialtiesService() {
   return new SearchSpecialtiesService(new PrismaSpecialtiesRepository());
